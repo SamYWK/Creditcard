6352d3c7965a250aa362834cd06b65d25f21ca5b
scan6 <pig840421@gmail.com>
2017-10-28 21:58:48 +0800

    under sampling

--- logreg.py
+++ logreg.py
@@ -7,9 +7,23 @@ Created on Tue Oct 24 17:07:04 2017
 
 import numpy as np
 import pandas as pd
+import sklearn.preprocessing
 
+#load data
 df = pd.read_csv('creditcard.csv')
-y = df['Class']
-#df = df.drop("Class", axis = 1)
-#print((df.Class == 1).describe())
-print(df[df.Class==0])
+normal_index = np.array(df[df.Class==0].index)
+fraud_index = np.array(df[df.Class==1].index)
+#under sampling
+random_normal_index = np.random.choice(normal_index, len(fraud_index), replace = False)
+under_sample_index = np.concatenate([random_normal_index, fraud_index])
+under_sample_df = df.iloc[under_sample_index, :]
+
+y = under_sample_df['Class']
+X = under_sample_df.drop(['Class'], axis = 1)
+#
+#normalization
+min_max_scaler = sklearn.preprocessing.MinMaxScaler()
+X['normalized_Amount'] = min_max_scaler.fit_transform(X['Amount'].values.reshape(-1,1))
+X = X.drop(['Amount'], axis = 1)
+print(X)
+
