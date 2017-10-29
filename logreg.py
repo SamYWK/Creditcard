deee47e3e0c61c3121fd072a7c57001ffe8c5989
scan6 <pig840421@gmail.com>
2017-10-29 22:13:15 +0800

    added logistic regressison

--- logreg.py
+++ logreg.py
@@ -8,6 +8,9 @@ Created on Tue Oct 24 17:07:04 2017
 import numpy as np
 import pandas as pd
 import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import recall_score
 
 #load data
 df = pd.read_csv('creditcard.csv')
@@ -25,5 +28,13 @@ X = under_sample_df.drop(['Class'], axis = 1)
 min_max_scaler = sklearn.preprocessing.MinMaxScaler()
 X['normalized_Amount'] = min_max_scaler.fit_transform(X['Amount'].values.reshape(-1,1))
 X = X.drop(['Amount'], axis = 1)
-print(X)
 
+#split 80% for training , 20% for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state = 0)
+
+#logistic regression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+logreg_predict = logreg.predict(X_test)
+
+print(recall_score(y_test.values, logreg_predict, average = 'binary'))    
